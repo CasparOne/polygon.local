039e55383d420f6ee099312e725df6c4146d882c
CasparOne <vvolodin.victor@gmail.com>
2019-05-02 03:19:51 +0300

    Added SoftDeletes feature
    Signed-off-by: CasparOne <vvolodin.victor@gmail.com>

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class BlogCategory
+ * @package App\Models
+ */
 class BlogCategory extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * @var array
+     */
+    protected $fillable
+        = [
+            'title',
+            'slug',
+            'parent_id',
+            'description'
+        ];
 }
