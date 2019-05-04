a7a61a00770d524b5aeb7cd7e55abf333464f63e
CasparOne <vvolodin.victor@gmail.com>
2019-05-05 01:01:52 +0300

    Laravel 5. migration INIT commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(BlogCategoriesSeeder::class);
-        factory(\App\Models\BlogPost::class, 400)->create();
+        // $this->call(UsersTableSeeder::class);
     }
 }
